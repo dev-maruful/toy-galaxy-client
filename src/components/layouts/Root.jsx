cf036e4bea4426150dc154dff5a252a5c5722415
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-18 10:24:38 +0600

    header and footer setup done

--- src/components/layouts/Root.jsx
+++ src/components/layouts/Root.jsx
@@ -7,7 +7,9 @@ const Root = () => {
   return (
     <>
       <Header></Header>
-      <Outlet></Outlet>
+      <div className="min-h-[calc(100vh-368px)]">
+        <Outlet></Outlet>
+      </div>
       <Footer></Footer>
     </>
   );
