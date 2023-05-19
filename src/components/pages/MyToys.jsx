b7ed1d1560f03988347cca6c52f926bf17bd80d8
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-19 20:26:41 +0600

    my toys page design done and data loaded successful

--- src/components/pages/MyToys.jsx
+++ src/components/pages/MyToys.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import { Link } from "react-router-dom";
 
 const MyToys = () => {
   const { user } = useContext(AuthContext);
@@ -11,13 +12,66 @@ const MyToys = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setMyToys(data);
       });
-  }, []);
+  }, [url]);
 
   return (
-    <div>
-      <h3>my toys are here</h3>
+    <div className="mt-10 mb-28">
+      <div className="overflow-x-auto">
+        <table className="table table-compact w-full">
+          <thead>
+            <tr>
+              <th></th>
+
+              <th className="text-xl font-semibold text-center">Toy Name</th>
+              <th className="text-xl font-semibold text-center">Toy Image</th>
+
+              <th className="text-xl font-semibold text-center">Price</th>
+              <th className="text-xl font-semibold text-center">
+                Available Quantity
+              </th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {myToys.map((singleToyDetails, index) => (
+              <tr key={singleToyDetails?._id}>
+                <th className="text-lg">{index + 1}</th>
+                <td className="text-lg font-medium text-center">
+                  {singleToyDetails?.name}
+                </td>
+                <td className="text-center">
+                  <div className="avatar">
+                    <div className="w-24 rounded">
+                      <img src={singleToyDetails?.photo} />
+                    </div>
+                  </div>
+                </td>
+                <td className="text-lg font-medium text-center">
+                  ${singleToyDetails?.price}
+                </td>
+                <td className="text-lg font-medium text-center">
+                  ${singleToyDetails?.availableQuantity}
+                </td>
+                <td className="text-lg font-medium text-center">
+                  <Link to={`/toyDetails/${singleToyDetails?._id}`}>
+                    <button className="btn btn-outline btn-primary">
+                      Update
+                    </button>
+                  </Link>
+                </td>
+                <td>
+                  <Link to={`/toyDetails/${singleToyDetails?._id}`}>
+                    <button className="btn btn-error">Delete</button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/components/pages/ToyFullDetails.jsx
+++ src/components/pages/ToyFullDetails.jsx
@@ -1,11 +1,55 @@
 import React from "react";
+import { FaRegStar, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
 import { useLoaderData } from "react-router-dom";
 
 const ToyFullDetails = () => {
   const toyDetails = useLoaderData();
   console.log(toyDetails);
+  const {
+    availableQuantity,
+    description,
+    name,
+    price,
+    photo,
+    rating,
+    sellerEmail,
+    sellerName,
+    subcategory,
+    _id,
+  } = toyDetails;
 
-  return <div>hello</div>;
+  return (
+    <div className="mt-10 mb-28">
+      <div className="card card-side bg-base-100 shadow-xl max-w-3xl mx-auto">
+        <figure className="w-2/3">
+          <img className="h-[500px]" src={photo} alt="" />
+        </figure>
+        <div className="flex flex-col justify-center px-5">
+          <h2 className="card-title text-2xl font-bold mb-3">
+            Toy name : {name}
+          </h2>
+          <p className="font-medium mb-2">Seller Name : {sellerName}</p>
+          <p className="font-medium mb-2">Seller Email : {sellerEmail}</p>
+          <p className="font-medium mb-2">Price : ${price}</p>
+          <p className="font-medium mb-2">
+            Available Quantity : {availableQuantity} pcs
+          </p>
+          <p className="font-medium mb-2">Description : {description}</p>
+          <div className="flex gap-2">
+            <p className="font-medium">Ratings : {rating}</p>
+            <Rating
+              className="text-warning text-lg"
+              emptySymbol={<FaRegStar></FaRegStar>}
+              fullSymbol={<FaStar></FaStar>}
+              initialRating={rating}
+              readonly
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ToyFullDetails;
